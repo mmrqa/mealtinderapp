33b8932ce94c977526d5cca9190ee366e5592174
Marvin Marqua <marvinmarqua@gmail.com>
2023-03-19 23:00:50 +0100

    added basic input for new recipe

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,21 +1,65 @@
-import { View, Text, StyleSheet } from "react-native"
+import React from 'react';
+import {
+    View,
+    KeyboardAvoidingView,
+    TextInput,
+    StyleSheet,
+    Text,
+    Platform,
+    TouchableWithoutFeedback,
+    Button,
+    Keyboard,
+} from 'react-native';
 
 import ColorStyleSheet from '../../components/ColorStyleSheet';
 
 const styles = StyleSheet.create({
-    container:{
-        flex: 1, 
+    container: {
+        flex: 1,
         justifyContent: 'center',
-        alignItems: 'center', 
+        alignItems: 'center',
         backgroundColor: ColorStyleSheet.primaryBackground.backgroundColor,
-    }
+    },
+    inner: {
+        padding: 24,
+    },
+    header: {
+        fontSize: 36,
+        marginBottom: 48,
+    },
+    textInput: {
+        height: 40,
+        borderColor: '#000000',
+        borderBottomWidth: 1,
+        marginBottom: 15,
+    },
+    btnContainer: {
+        backgroundColor: 'white',
+        marginTop: 12,
+    },
 });
 
 export default function HomeScreen() {
     return (
-        <View style={styles.container}>
-            <Text>Hi</Text>
-        </View>
+        <KeyboardAvoidingView
+            behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+            style={styles.container}>
+            <TouchableWithoutFeedback onPress={Platform.OS === "web" ? null : Keyboard.dismiss}>
+                <View style={styles.inner}>
+                    <Text style={styles.header}>Add new recipe</Text>
+                    <TextInput placeholder="Name" style={styles.textInput} />
+                    <TextInput editable multiline={true}
+                        maxLength={253} placeholder="Description" style={styles.textInput} />
+                    <TextInput placeholder="Link" style={styles.textInput} />
+                    <TextInput placeholder="Duration" style={styles.textInput} />
+                    <TextInput placeholder="image" style={styles.textInput} />
+                    
+                    <View style={styles.btnContainer}>
+                        <Button title="Add" onPress={() => null} />
+                    </View>
+                </View>
+            </TouchableWithoutFeedback>
+        </KeyboardAvoidingView>
     )
 
 }
