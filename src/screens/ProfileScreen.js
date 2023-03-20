ac2355f04ed04142bcf3c8f0f772e44da90cfd51
MoritzSchwoerer <mo.schwoerer@outlook.com>
2023-03-20 21:29:46 +0100

    Added function with alerts when klicking buttons

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,40 +1,80 @@
-import { View, Text, StyleSheet} from "react-native"
+import { View, Text, StyleSheet, Pressable } from "react-native"
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import ColorStyleSheet from "../../components/ColorStyleSheet"
 
 export default function ProfileScreen() {
+    function onPressAccount() {
+        alert("Account")
+    }
+
+    function onPressSettings() {
+        alert("Settings")
+    }
+
+    function onPressHelp() {
+        alert("Help")
+    }
+
+    function onPressAbout() {
+        alert("About")
+    }
+
+    function onPressSubscribtion() {
+        alert("Subscribtion")
+    }
+
+
     return (
         <View style={styles.mainViewContainer}>
-            <View style={styles.boxContainer}>
-                <View style={styles.box}>
-                    <MaterialCommunityIcons name="account" color="white" size={50} />
-                    <Text style={styles.boxText}>Profile</Text>
+            <Pressable onPress={onPressAccount}>
+                <View style={styles.boxContainer}>
+                    <View style={styles.topBox}>
+                        <MaterialCommunityIcons name="account" color="white" size={50} />
+                        <Text style={styles.boxText}>Profile</Text>
+                        <MaterialCommunityIcons name="chevron-right" color="white" size={50} />
+                    </View>
                 </View>
-            </View>
-            <View style={styles.boxContainer}>
-                <View style={styles.box}>
-                    <MaterialCommunityIcons name="cog" color="white" size={50} />
-                    <Text style={styles.boxText}>Settings</Text>
+            </Pressable>
+
+            <Pressable onPress={onPressSettings}>
+                <View style={styles.boxContainer}>
+                    <View style={styles.box}>
+                        <MaterialCommunityIcons name="cog" color="white" size={50} />
+                        <Text style={styles.boxText}>Settings</Text>
+                        <MaterialCommunityIcons name="chevron-right" color="white" size={50} />
+                    </View>
                 </View>
-            </View>
-            <View style={styles.boxContainer}>
-                <View style={styles.box}>
-                    <MaterialCommunityIcons name="help-circle" color="white" size={50} />
-                    <Text style={styles.boxText}>Help</Text>
+            </Pressable>
+
+            <Pressable onPress={onPressHelp}>
+                <View style={styles.boxContainer}>
+                    <View style={styles.box}>
+                        <MaterialCommunityIcons name="help-circle" color="white" size={50} />
+                        <Text style={styles.boxText}>Help</Text>
+                        <MaterialCommunityIcons name="chevron-right" color="white" size={50} />
+                    </View>
                 </View>
-            </View>
-            <View style={styles.boxContainer}>
-                <View style={styles.box}>
-                    <MaterialCommunityIcons name="information" color="white" size={50} />
-                    <Text style={styles.boxText}>About</Text>
+            </Pressable>
+
+            <Pressable onPress={onPressAbout}>
+                <View style={styles.boxContainer}>
+                    <View style={styles.box}>
+                        <MaterialCommunityIcons name="information" color="white" size={50} />
+                        <Text style={styles.boxText}>About</Text>
+                        <MaterialCommunityIcons name="chevron-right" color="white" size={50} />
+                    </View>
                 </View>
-            </View>
-            <View style={styles.boxContainer}>
-                <View style={styles.box}>
-                    <MaterialCommunityIcons name="credit-card" color="white" size={50} />
-                    <Text style={styles.boxText}>Subscribtion</Text>
+            </Pressable>
+
+            <Pressable onPress={onPressSubscribtion}>
+                <View style={styles.boxContainer}>
+                    <View style={styles.bottomBox}>
+                        <MaterialCommunityIcons name="credit-card" color="white" size={50} />
+                        <Text style={styles.boxText}>Subscribtion</Text>
+                        <MaterialCommunityIcons name="chevron-right" color="white" size={50} />
+                    </View>
                 </View>
-            </View>
+            </Pressable>
         </View>
     );
 }
@@ -44,23 +84,59 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: ColorStyleSheet.primaryBackground.backgroundColor,
         paddingTop: 100,
-        justifyContent: "flex-end",   
     },
     boxContainer: {
         flexDirection: "row",
-        alignContent: "space-between",
     },
-    box: {
+    topBox: {
         flex: 1,
+        flexDirection: "row",
+        justifyContent: "space-between",
+        padding: 5,
         backgroundColor: "black",
         alignContent: "center",
         alignItems: "center",
-        justifyContent: "center",
-        borderRadius: 20,
-        margin: 10,
-        padding: 10,
+        borderTopLeftRadius: 20,
+        borderTopRightRadius: 20,
         borderColor: "white",
-        borderWidth: 3,
+        borderTopWidth: 3,
+        borderLeftWidth: 3,
+        borderRightWidth: 3,
+        borderBottomWidth: 3,
+        shadowColor: 'black',
+        shadowOpacity: 0.2,
+        shadowRadius: 20,
+    },
+    box: {
+        flex: 1,
+        flexDirection: "row",
+        justifyContent: "space-between",
+        padding: 5,
+        backgroundColor: "black",
+        alignContent: "center",
+        alignItems: "center",
+        borderBottomWidth: 3,
+        borderLeftWidth: 3,
+        borderRightWidth: 3,
+        borderColor: "white",
+        shadowColor: 'black',
+        shadowOpacity: 0.2,
+        shadowRadius: 20,
+    },
+    bottomBox: {
+        flex: 1,
+        flexDirection: "row",
+        justifyContent: "space-between",
+        padding: 5,
+        backgroundColor: "black",
+        alignContent: "center",
+        alignItems: "center",
+        borderBottomLeftRadius: 20,
+        borderBottomRightRadius: 20,
+        borderColor: "white",
+        borderLeftWidth: 3,
+        borderRightWidth: 3,
+        borderBottomWidth: 3,
         shadowColor: 'black',
         shadowOpacity: 0.2,
         shadowRadius: 20,
