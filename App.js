a5976199a82e2c8722876f39219e97d42aef37e3
MoritzSchwoerer <mo.schwoerer@outlook.com>
2023-03-21 22:02:47 +0100

    Solving bug where navigation bar flickers when switching screens

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import HomeScreen from './src/screens/HomeScreen';
 import ChooseMealScreen from './src/screens/ChooseMealScreen';
-import {createMaterialBottomTabNavigator} from '@react-navigation/material-bottom-tabs';
-import {NavigationContainer} from '@react-navigation/native';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
 import ProfileScreen from './src/screens/ProfileScreen';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import { DefaultTheme, Provider } from 'react-native-paper';
+
 
 const Tab = createMaterialBottomTabNavigator();
 
@@ -12,42 +13,45 @@ import ColorStyleSheet from './components/ColorStyleSheet';
 
 function App() {
   const activeColor = ColorStyleSheet.primaryBackground.backgroundColor;
-  const inactiveColor = 'white';
 
   return (
     <Provider theme={theme}>
       <NavigationContainer>
-        <Tab.Navigator 
+        <Tab.Navigator
           initialRouteName='Go'
-          activeColor={activeColor} 
-          inactiveColor={inactiveColor} 
-          barStyle={{ backgroundColor: 'black'}}
-          tabBarIcon={false}
-          shifting={true}
+          activeColor={activeColor}
+          inactiveColor='white'
+          barStyle={{
+            backgroundColor: 'black',
+
+          }}
+
           labeled={false}
+
         >
           <Tab.Screen name="+" component={HomeScreen} options={{
             tabBarColor: 'black',
-            tabBarIcon: ({color}) => (
+            tabBarIcon: ({ color }) => (
               <MaterialCommunityIcons name="plus" color={color} size={26} />
             ),
-          }}/>
+          }} />
           <Tab.Screen name="Go" component={ChooseMealScreen} options={{
             tabBarColor: 'black',
-            tabBarIcon: ({color}) => (
+            tabBarIcon: ({ color }) => (
               <MaterialCommunityIcons name="food" color={color} size={26} />
             ),
-          }}/>
+          }} />
           <Tab.Screen name="Profile" component={ProfileScreen} options={{
             tabBarColor: 'black',
-            tabBarIcon: ({color}) => (
+            tabBarIcon: ({ color }) => (
               <MaterialCommunityIcons name="account" color={color} size={26} />
             ),
-          }}/>
+          }} />
 
         </Tab.Navigator>
       </NavigationContainer>
     </Provider>
+
   );
 }
 
@@ -58,5 +62,6 @@ const theme = {
     secondaryContainer: 'transparent', // Use transparent to disable the little highlighting oval
   },
 };
+
 
 export default App;
